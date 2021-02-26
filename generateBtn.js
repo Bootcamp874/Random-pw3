40215118463bd1dd7200e0e89062f2885d077e07
Bootcamp874 <sandradltrahyatt@gmail.com>
2021-02-25 23:30:00 -0800

    more work for hw

--- generateBtn.js
+++ generateBtn.js
@@ -14,15 +14,20 @@ var integerNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var biggieLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var littleLetter =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var newArray = [];
-
+var passCode
 
 
 
 function generatePassword() {
-var gettingInput = promptUser();
-console.log(gettingInput);
 
-
+  
+  
+  
+  
+  var gettingInput = promptUser();
+  console.log(gettingInput);
+  
+  
 }
 
 function promptUser(){
@@ -46,37 +51,43 @@ function promptUser(){
   }
   return userAnswer;
   
-  
-  function gettingRandom () {
-    if (wantsNumber === true) {
-    var getThatNum = Math.floor(Math.random() * integerNum.length);
+}
+
+function gettingRandom () {
+  // if (wantsNumber === true) {
+    // var getThatNum = Math.floor(Math.random() * integerNum.length);
     
-    } if (specialChar === true) {
-      var symbolPick = Math.floor(Math.random() * specialCharacter.length);
-  
-    } if (bigLetter === true) {
-      var getCapital = Math.floor(math.random() * biggieLetter.length);
-  
-    } if (smallLetter === true) {
-      var gettingSmall = Math.floor(Math.random() * littleLetter.length);
+    // } if (specialChar === true) {
+      //   var symbolPick = Math.floor(Math.random() * specialCharacter.length);
       
-    } 
-}
+      // } if (bigLetter === true) {
+        //   var getCapital = Math.floor(math.random() * biggieLetter.length);
+        
+        // } if (smallLetter === true) {
+          //   var gettingSmall = Math.floor(Math.random() * littleLetter.length);
+          
+          // } 
+          
+          
+          if (wantsNumber) {
+            var newArray = [].push(integerNum);
+          } if (specialChar) {
+            newArray = [].concat(specialCharacter);
+          } if (bigLetter) {
+            newArray = [].concat(biggieLetter);
+          } if (smallLetter) {
+            newArray = [].concat(littleLetter);
+          }
+          return newArray;
+        }
 
-if (wantsNumber) {
-newArray = [].concat(integerNum);
-} if (specialChar) {
-    newArray = [].concat(specialCharacter);
-} if (bigLetter) {
-  newArray = [].concat(biggieLetter);
-} if (smallLetter) {
-  newArray = [].concat(littleLetter);
-}
 
 
-
-
-
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+          for (i =0; i < pwLength; i++) {
+            var tryingTogetpw = Math.floor(Math.random() * newArray.length);
+          }
+        
+        
+        
+        // Add event listener to generate button
+        generateBtn.addEventListener("click", writePassword);
